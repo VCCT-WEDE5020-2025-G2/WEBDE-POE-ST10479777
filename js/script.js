341c0820d0d537804657e32089fc369a3af32793
ST10479777 <ST10479777@vcconnect.edu.za>
2025-10-30 03:21:48 +0200

    added an interactive map

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ menuOpenButton.addEventListener("click", () => {
 });
 
 menuCloseButton.addEventListener("click", () => menuOpenButton.click()); 
+// Leaflet Map
+var map = L.map('map').setView([-33.918861, 18.423300], 13); // Cape Town coordinates
+
+// map layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// marker for the coffee shop
+L.marker([-33.918861, 18.423300]).addTo(map)
+    .bindPopup("<b>G&B Coffee</b><br>76 Bree Street, Cape Town")
+    .openPopup();
